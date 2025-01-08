5d7ee59705bdb373abfaa4667c18aa03a6a1ad2a
Julio Raynier <raynier97@hotmail.com>
2025-01-08 14:28:05 +0100

    made routes for all components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { IngredientsComponent } from './ingredients/ingredients.component';
+import { LoginComponent } from './login/login.component';
+import { MealsComponent } from './meals/meals.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { SearchComponent } from './search/search.component';
+import { UserComponent } from './user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'ingredients', component: IngredientsComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'meals', component: MealsComponent },
+    { path: 'navbar', component: NavbarComponent },
+    { path: 'recipes', component: RecipesComponent },
+    { path: 'registration', component: RegistrationComponent },
+    { path: 'search', component: SearchComponent },
+    { path: 'user', component: UserComponent },
+];
