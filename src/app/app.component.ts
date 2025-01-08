6172ee7b2d0a54b38ed570aa8a50af97191959c1
Julio Raynier <raynier97@hotmail.com>
2025-01-08 14:30:30 +0100

    added NavbarComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ NavbarComponent ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true
 })
 export class AppComponent {
-  title = 'koolyhealth';
+  title = 'CarbWatch';
 }
