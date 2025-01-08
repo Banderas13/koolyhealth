a03c1e539001fdfc1f32e092d308fb33cdddb9d2
Julio Raynier <raynier97@hotmail.com>
2025-01-08 14:29:41 +0100

    imported RouterOutlet&Link and made a temp. list with different pages for testing purposes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [ RouterOutlet, RouterLink ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
+  standalone: true,
 })
 export class NavbarComponent {
 
