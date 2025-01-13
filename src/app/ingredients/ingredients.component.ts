1de7e2baafab4d7f1a8b1bc8075f3b3a48821315
Julio Raynier <raynier97@hotmail.com>
2025-01-13 10:54:40 +0100

    added functionality to fetch, dispaly and save to local storage ingredients on the ingredient and search component

--- src/app/ingredients/ingredients.component.ts
+++ src/app/ingredients/ingredients.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+// ingredients.component.ts
+import { Component, OnInit } from '@angular/core';
+import { IngredientService } from '../shared/ingredient.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ingredients',
-  imports: [],
   templateUrl: './ingredients.component.html',
-  styleUrl: './ingredients.component.css'
+  styleUrl: './ingredients.component.css',
+  standalone: true,
+  imports: [FormsModule, CommonModule,],
 })
-export class IngredientsComponent {
+export class IngredientsComponent implements OnInit {
+  selectedIngredients: any[] = [];
+  totalCarbs: number = 0;
 
+  constructor(private ingredientService: IngredientService) {}
+
+  ngOnInit() {
+    this.selectedIngredients = this.ingredientService.getSelectedIngredients();
+    this.calculateTotalCarbs();
+  }
+
+  getCarbohydrates(ingredient: any): string {
+    const carbs = ingredient.nutrition?.nutrients?.find(
+      (nutrient: any) => nutrient.name === 'Carbohydrates'
+    );
+    return carbs ? `${carbs.amount} ${carbs.unit}` : 'N/A';
+  }
+
+  openSearch() {
+    // Logic to open the search component, can be done using routing or modals
+  }
+
+  calculateTotalCarbs() {
+    this.totalCarbs = this.ingredientService.calculateTotalCarbs();
+    console.log(this.totalCarbs)
+  }
 }
