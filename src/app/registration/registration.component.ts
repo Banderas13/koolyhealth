362f5d96db19f00bb4b603f55e59587285fd26e9
Julio Raynier <raynier97@hotmail.com>
2025-01-14 15:05:57 +0100

    Added basic template to registration & added Registration functionality

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
+import { UserService } from '../shared/user.service';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  standalone: true,
+  imports: [ FormsModule, RouterLink],
+  providers: [ UserService],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
+  username!: string;
+  password!: string;
+  firstName!: string;
+  lastName!: string;
+  bdate!: any;
+  carbeffect!: number;
+  insuline!: number;
 
+
+  constructor(
+    private UserService: UserService,
+    private router: Router
+  ) {}
+
+  async onSubmit() {
+    (await this.UserService.register(
+      this.username,
+      this.password,
+      this.firstName,
+      this.lastName,
+      this.bdate,
+      this.carbeffect,
+      this.insuline
+    )).subscribe(
+      (response) => {
+        alert('User registered successfully!');
+        this.router.navigate(['/login']);
+      },
+      (error) => {
+        console.error('Error registering user:', error);
+        alert('An error occurred while registering. Please try again.');
+      }
+    );
+  }
 }
