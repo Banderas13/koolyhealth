52593cf3717772cdce967619a3b5a488a77e6521
Julio Raynier <raynier97@hotmail.com>
2025-01-21 16:39:25 +0100

    created a multi page register form & moved the register functionality to their respective sub components. Used main component for displaying/toggling between steps

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,49 +1,82 @@
+// import { Component } from '@angular/core';
+// import { UserService } from '../shared/user.service';
+// import { FormsModule } from '@angular/forms';
+// import { Router, RouterLink } from '@angular/router';
+// import { PersonalDataComponent } from "./personal-data/personal-data.component";
+// import { InsulineDataComponent } from "./insuline-data/insuline-data.component";
+
+// @Component({
+//   selector: 'app-registration',
+//   standalone: true,
+//   imports: [FormsModule, RouterLink, PersonalDataComponent, InsulineDataComponent],
+//   providers: [ UserService],
+//   templateUrl: './registration.component.html',
+//   styleUrl: './registration.component.css'
+// })
+// export class RegistrationComponent {
+//   username!: string;
+//   password!: string;
+//   firstName!: string;
+//   lastName!: string;
+//   bdate!: any;
+//   carbeffect!: number;
+//   insuline!: number;
+
+
+//   constructor(
+//     private UserService: UserService,
+//     private router: Router
+//   ) {}
+
+//   async onSubmit() {
+//     (await this.UserService.register(
+//       this.username,
+//       this.password,
+//       this.firstName,
+//       this.lastName,
+//       this.bdate,
+//       this.carbeffect,
+//       this.insuline
+//     )).subscribe({
+//       next: (response) => {
+//         alert('User registered successfully!');
+//         this.router.navigate(['/login']);
+//       },
+//       error: (error) => {
+//         console.error('Error registering user:', error);
+//         alert('An error occurred while registering. Please try again.');
+//       }
+//     });
+//   }
+// }
+
+
 import { Component } from '@angular/core';
-import { UserService } from '../shared/user.service';
-import { FormsModule } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { PersonalDataComponent } from './personal-data/personal-data.component';
+import { InsulineDataComponent } from './insuline-data/insuline-data.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registration',
-  standalone: true,
-  imports: [ FormsModule, RouterLink],
-  providers: [ UserService],
   templateUrl: './registration.component.html',
-  styleUrl: './registration.component.css'
+  styleUrls: ['./registration.component.css'],
+  imports: [PersonalDataComponent, InsulineDataComponent, CommonModule],
 })
 export class RegistrationComponent {
-  username!: string;
-  password!: string;
-  firstName!: string;
-  lastName!: string;
-  bdate!: any;
-  carbeffect!: number;
-  insuline!: number;
+  currentStep = 1;
+  formData: any = {};
 
+  moveToNextStep(data: any) {
+    this.formData = { ...this.formData, ...data };
+    this.currentStep++;
+  }
 
-  constructor(
-    private UserService: UserService,
-    private router: Router
-  ) {}
+  moveToPreviousStep() {
+    this.currentStep--;
+  }
 
-  async onSubmit() {
-    (await this.UserService.register(
-      this.username,
-      this.password,
-      this.firstName,
-      this.lastName,
-      this.bdate,
-      this.carbeffect,
-      this.insuline
-    )).subscribe(
-      (response) => {
-        alert('User registered successfully!');
-        this.router.navigate(['/login']);
-      },
-      (error) => {
-        console.error('Error registering user:', error);
-        alert('An error occurred while registering. Please try again.');
-      }
-    );
+  handleSubmission(response: any) {
+    console.log('Registration successful!', response);
+    // Handle successful registration, e.g., show a success message or navigate
   }
 }
