41c3fbf9587f11611b3e3bb5515a0f48838186a4
Seth_Poelmans <seth.poelmans@gmail.com>
2025-01-21 10:22:30 +0100

    Meals worden nu ingeladen op de meals page

--- src/app/meals/meals.component.ts
+++ src/app/meals/meals.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RecipeService } from '../shared/recipe.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-meals',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './meals.component.html',
   styleUrl: './meals.component.css'
 })
 export class MealsComponent {
+
+  constructor(private recipeService: RecipeService){}
+
+  recepies:any;
+
+  GetRecipes(){
+    this.recipeService.FetchData();
+
+}
+
+ngOnInit() {
+  this.GetRecipes();
+  this.recepies =  this.recipeService.recepies
+}
 }
--- src/app/shared/recipe.service.ts
+++ src/app/shared/recipe.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable, output, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private apiUrl = 'http://127.0.0.1:8000/api/courses';
+  recepies = signal([]);
+
+  FetchData(){
+    fetch(this.apiUrl)
+    .then(res => res.json())
+    .then(data => {
+      this.recepies.set(data);
+    })
+    .catch(e => console.log(e));
+  }
 }
